9759c4d1f18375828454ef7307e011c7aa15bc07
Erik Cohen <ejcohe22@colby.edu>
2023-01-21 16:13:58 -0600

    themeing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,19 @@
-function app() {
-  return (
-    <div className="App">
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseLine, ThemeProvider} from "@mui/material";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="App">
+          <main className="conent"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
-export default app;
+export default App;
